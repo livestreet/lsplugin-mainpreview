895d513abfde90bf1ea87a4e8b74c289c3bc611d
gigabyte <1099511627776@mail.ru>
2013-08-16 15:40:27 +0300

    Imaplemented Ajax Convert and Reconvert

--- classes/actions/ActionMain.class.php
+++ classes/actions/ActionMain.class.php
@@ -17,72 +17,108 @@
 
 
 class PluginMainpreview_ActionMain extends ActionPlugin {
-	/**
-	 * Инициализация
-	 *
-	 * @return null
-	 */
-	public function Init() {
-		/**
-		 * Если пользователь не авторизован и не админ, то выкидываем его
-		 */
-		$this->oUserCurrent=$this->User_GetUserCurrent();
-		if (!$this->oUserCurrent or !$this->oUserCurrent->isAdministrator()) {
-			return $this->EventNotFound();
-		}
-	}
+    /**
+     * Инициализация
+     *
+     * @return null
+     */
+    public function Init() {
+        /**
+         * Если пользователь не авторизован и не админ, то выкидываем его
+         */
+        $this->oUserCurrent=$this->User_GetUserCurrent();
+        if (!$this->oUserCurrent or !$this->oUserCurrent->isAdministrator()) {
+            return $this->EventNotFound();
+        }
+    }
 
-	protected function RegisterEvent() {
-		$this->AddEvent('convert','EventConvert');
-		$this->AddEvent('reconvert','EventReConvert');
-	}
+    protected function RegisterEvent() {
+        $this->AddEvent('convert','EventConvert');
+        $this->AddEvent('reconvert','EventReConvert');
+        $this->AddEvent('ajaxconvert','EventAjaxConvert');
+        $this->AddEvent('ajaxreconvert','EventAjaxReConvert');
+    }
 
 
-	/**********************************************************************************
-	 ************************ РЕАЛИЗАЦИЯ ЭКШЕНА ***************************************
-	 **********************************************************************************
-	 */
+    /**********************************************************************************
+     ************************ РЕАЛИЗАЦИЯ ЭКШЕНА ***************************************
+     **********************************************************************************
+     */
 
-	/**
-	 * Автоматическое создание превью для топиков
-	 */
-	protected function EventConvert() {
-		if (isPost('submit_convert')) {
-			$this->Security_ValidateSendForm();
-			set_time_limit(0);
+    /**
+     * Автоматическое создание превью для топиков
+     */
+    protected function EventConvert() {
+        if (isPost('submit_convert')) {
+            $this->Security_ValidateSendForm();
+            set_time_limit(0);
 
-			$iPage=1;
-			$iCount=0;
-			while ($aTopics=$this->PluginMainpreview_Main_GetTopics($iPage,20)) {
-				$iPage++;
-				foreach($aTopics as $oTopic) {
-					$this->PluginMainpreview_Main_SaveTopic($oTopic);
-					$iCount++;
-				}
-			}
-			$this->Message_AddNotice($this->Lang_Get('plugin.mainpreview.convert_end',array('count_topic'=>$iCount)));
-		}
-	}
+            $iPage=1;
+            $iCount=0;
+            while ($aTopics=$this->PluginMainpreview_Main_GetTopics($iPage,20)) {
+                $iPage++;
+                foreach($aTopics as $oTopic) {
+                    $this->PluginMainpreview_Main_SaveTopic($oTopic);
+                    $iCount++;
+                }
+            }
+            $this->Message_AddNotice($this->Lang_Get('plugin.mainpreview.convert_end',array('count_topic'=>$iCount)));
+        }
+    }
 
-	/**
-	 * Переконвертация превью к новым размерам
-	 */
-	protected function EventReConvert() {
-		if (isPost('submit_reconvert')) {
-			$this->Security_ValidateSendForm();
-			set_time_limit(0);
+    protected function EventAjaxConvert(){
+        $this->Viewer_SetResponseAjax('json');
+        $this->Security_ValidateSendForm();
+        $iPage = $this->GetParam(0);
+        $iCount = 0;
+        if($aTopics=$this->PluginMainpreview_Main_GetTopics($iPage,20)){
+            foreach($aTopics as $oTopic) {
+                $this->PluginMainpreview_Main_SaveTopic($oTopic);
+                $iCount++;
+            }
+            $this->Message_AddNotice($this->Lang_Get('plugin.mainpreview.convert_end',array('count_topic'=>$iCount,'page_num'=>$iPage)));
+            return;
+        } else {
+            $this->Message_AddError($this->Lang_Get('plugin.mainpreview.convert_end',array('count_topic'=>$iCount,'page_num'=>$iPage)));
+        }
+    }
 
-			$iPage=1;
-			$iCount=0;
-			while ($aTopics=$this->PluginMainpreview_Main_GetTopics($iPage,30,array())) {
-				$iPage++;
-				foreach($aTopics as $oTopic) {
-					$this->PluginMainpreview_Main_ReMakePreview($oTopic);
-					$iCount++;
-				}
-			}
-			$this->Message_AddNotice($this->Lang_Get('plugin.mainpreview.reconvert_end',array('count_topic'=>$iCount)));
-		}
-	}
+    /**
+     * Переконвертация превью к новым размерам
+     */
+    protected function EventReConvert() {
+        if (isPost('submit_reconvert')) {
+            $this->Security_ValidateSendForm();
+            set_time_limit(0);
+
+            $iPage=1;
+            $iCount=0;
+            while ($aTopics=$this->PluginMainpreview_Main_GetTopics($iPage,30,array())) {
+                $iPage++;
+                foreach($aTopics as $oTopic) {
+                    $this->PluginMainpreview_Main_ReMakePreview($oTopic);
+                    $iCount++;
+                }
+            }
+            $this->Message_AddNotice($this->Lang_Get('plugin.mainpreview.reconvert_end',array('count_topic'=>$iCount)));
+        }
+    }
+
+    protected function EventAjaxReConvert(){
+        $this->Viewer_SetResponseAjax('json');
+        $this->Security_ValidateSendForm();
+        $iPage = $this->GetParam(0);
+        $iCount = 0;
+        if($aTopics=$this->PluginMainpreview_Main_GetTopics($iPage,20)){
+            foreach($aTopics as $oTopic) {
+                $this->PluginMainpreview_Main_ReMakePreview($oTopic);
+                $iCount++;
+            }
+            $this->Message_AddNotice($this->Lang_Get('plugin.mainpreview.convert_end',array('count_topic'=>$iCount,'page_num'=>$iPage)));
+            return;
+        } else {
+            $this->Message_AddError($this->Lang_Get('plugin.mainpreview.convert_end',array('count_topic'=>$iCount,'page_num'=>$iPage)));
+        }
+    }
 }
 ?>
